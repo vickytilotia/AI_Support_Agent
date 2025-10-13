46cd16893be13078338cd661e7a7eac82cdb96fe
Vivek Kumar <vickytilotia@gmail.com>
2025-10-13 10:52:38 +0530

    added ollama mistral llm responder

--- app/api/routes_chat.py
+++ app/api/routes_chat.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter
-from app.services.rag_engine import RAGEngine
+# from app.services.rag_engine import RAGEngine
+from app.services.llm_agent import LLMResponder
 
 router = APIRouter()
-rag  = RAGEngine()
+# rag  = RAGEngine()
+agent = LLMResponder()
 
-@router.on_event("startup")
-def load_data():
-    rag.load_faqs()
-    rag.load_pdfs()
+# @router.on_event("startup")
+# def load_data():
+#     rag.load_faqs()
+#     rag.load_pdfs()
 
 @router.post("/")
 async def chat_endpoint(query: str):
-    results = rag.query(query)
-    return {"query": query, "retrieved_context": results}
+    answer = agent.generate_answer(query)
+    return {"query": query, "answer": answer}
---
+++ app/services/llm_agent.py
@@ -0,0 +1,33 @@
+from langchain_community.llms import Ollama 
+from app.services.rag_engine import RAGEngine
+
+
+class LLMResponder:
+    def __init__(self):
+        self.llm = Ollama(model="mistral")   #local model
+        self.rag = RAGEngine()
+        self.rag.load_faqs()
+        self.rag.load_pdfs()
+
+    
+    def generate_answer(self, user_query: str):
+        """Retrieve context + generate final answer."""
+        context_docs = self.rag.query(user_query) 
+        context_text = "\n".join(context_docs)
+
+        prompt = f""" 
+        You are an helpful AI customer support assistant. 
+        Use the following context to answer the user's query accurately.
+        If the context doesn't contain the answer, say "I'm not sure, would you like me to create a support ticket?".
+
+        CONTEXT:
+        {context_text}
+        USER QUERY:
+        {user_query}
+
+        RESPONSE:
+        """
+
+        response = self.llm.invoke(prompt)
+        print(response)
+        return response
