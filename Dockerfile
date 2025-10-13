06143ffe37a2e19c5d980d51e5f1c22522f5c647
Vivek Kumar <vickytilotia@gmail.com>
2025-10-13 13:29:14 +0530

    added docker, prometheus config

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Base image
+FROM python:3.11-slim
+
+# Set working directory
+WORKDIR /app
+
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libpq-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+# Copy requirements
+COPY requirements.txt .
+
+# Install Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy project
+COPY . .
+
+# Expose port
+EXPOSE 8000
+
+# Run FastAPI
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.api import routes_chat, routes_ticket
 from app.core.database import init_db
 from app.api import routes_analytics
+from prometheus_fastapi_instrumentator import Instrumentator
 import asyncio
 
 app = FastAPI(title="AI Support Agent", version="1.0.0")
@@ -9,6 +10,10 @@ app = FastAPI(title="AI Support Agent", version="1.0.0")
 app.include_router(routes_chat.router, prefix="/chat", tags=["Chat"])
 app.include_router(routes_ticket.router, prefix="/tickets", tags=["Tickets"])
 app.include_router(routes_analytics.router, prefix="/analytics", tags=["Analytics"])
+
+
+instrumentator = Instrumentator()
+instrumentator.instrument(app).expose(app, endpoint="/metrics")
 
 @app.on_event("startup")
 async def startup_event():
